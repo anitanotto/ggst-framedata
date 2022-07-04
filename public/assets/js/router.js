269e07d4085250caa9583b78a6d3e94790868ff8
hammock-sleeper <hammock_sleeper@tutanota.com>
2022-07-04 15:44:35 -0400

    update styling

--- public/assets/js/router.js
+++ public/assets/js/router.js
@@ -1,6 +1,6 @@
 let p1 = document.querySelector('#p1')
 let p2 = document.querySelector('#p2')
-let button = document.querySelector('#button')
+let button = document.querySelector('.button')
 
 button.href = `/turn/${p1.value}/${p2.value}/`
 
